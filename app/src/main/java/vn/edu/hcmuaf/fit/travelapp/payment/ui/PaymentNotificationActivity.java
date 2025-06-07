29cb3f02d55624cbcea2ee44d22f3342c80e64c2
HQLuat <22130157@st.hcmuaf.edu.vn>
2025-06-07 19:01:31 +0700

    create order directory

--- app/src/main/java/vn/edu/hcmuaf/fit/travelapp/payment/ui/PaymentNotificationActivity.java
+++ app/src/main/java/vn/edu/hcmuaf/fit/travelapp/payment/ui/PaymentNotificationActivity.java
@@ -3,16 +3,9 @@ package vn.edu.hcmuaf.fit.travelapp.payment.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import vn.edu.hcmuaf.fit.travelapp.R;
-import vn.edu.hcmuaf.fit.travelapp.databinding.ActivityPaymentBinding;
 import vn.edu.hcmuaf.fit.travelapp.databinding.ActivityPaymentNotificationBinding;
 import vn.edu.hcmuaf.fit.travelapp.product.home.ui.MainActivity;
 
